5918e3dc8ee602ac2587330375c61ae1e30ef8ba
Md Shafiqul Haque <shafiqulhaque1971@gmail.com>
2022-05-13 03:10:43 -0500

    W3D5 V-1

---
+++ W3D5/q1/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    let name = re.query.name;
+    if (!name) {
+        name = "person";
+    }
+    res.send(`Welcome ${name}`);
+});
+app.listen(3000);

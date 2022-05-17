4f9b6d8526395015cec0984f94a8ebcae48b5e1b
Md Shafiqul Haque <shafiqulhaque1971@gmail.com>
2022-05-17 02:01:58 -0500

    update

---
+++ W4D1/q1/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "templates/view"))
+
+
+app.get('/', (req, res) => {
+    res.render("tableList", {
+        cookies: req.cookies,
+        pageTitle: "Add Cookie Form"
+    });
+
+});
+
+
+app.post("/", (req, res) => {
+    if (req.body.key && req.body.value) {
+        res.cookie(req.body.key, req.body.value);
+    }
+    res.redirect(303, "/");
+})
+
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+})

5cfd8abee704ee86ab77775c52806b4168c51fdb
Md Shafiqul Haque <shafiqulhaque1971@gmail.com>
2022-05-23 16:18:35 -0500

    hass

--- W3D1-ClassComponents/account.js
+++ W3D1-ClassComponents/account.js
@@ -1,25 +1,13 @@
 "use strict";
 
-/**
- * A Bank Account class
- * 
- * Provides the basic functionality that every account should have
- */
 class Account {
-    /**
-     * Constructor for creating a new Account object
-     * 
-     * @param {number} number the number for this account
-     */
     constructor(number) {
         this._number = number; // the account number
         this._balance = 0.0;
     }
 
     /**
-     * Getter for the 'private' number field
-     * 
-     * @returns {number} the account number
+     * Getter for the 'private' number field   
      */
     getNumber() {
         return this._number;
@@ -27,8 +15,6 @@ class Account {
 
     /**
      * Accessor for the 'private' balance field
-     * 
-     * @returns {number} balance for this account
      */
     getBalance() {
         return this._balance;
@@ -36,10 +22,6 @@ class Account {
 
     /**
      * Method to add money into the account
-     * 
-     * @param {number} amount money to be deposited into the account 
-     * @returns {undefined}
-     * @throws {RangeError} when amount is less than or equal to zero
      */
     deposit(amount) {
         if (amount <= 0) {
@@ -50,11 +32,6 @@ class Account {
 
     /**
      * Method to take money out of the account
-     * 
-     * @param {number} amount money to be taken out of the account
-     * @returns {undefined}
-     * @throws {RangeError} when amount is less than or equal to zero
-     * @throws {Error} when the account has insufficient funds (balance)
      */
     withdraw(amount) {
         if (amount <= 0) {
@@ -66,17 +43,12 @@ class Account {
         this._balance -= amount;
     }
 
-    /**
-     * @returns {string} representation of this account
-     */
     toString() {
         return "Account " + this._number + ": balance " + this._balance;
     }
 
     /**
      * Performs needed actions at the end of the month
-     * 
-     * @returns {undefined}
      */
     endOfMonth() {
         return ""; // does nothing
